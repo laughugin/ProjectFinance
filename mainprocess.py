5e09d21bd767ce06c8c0028d3509c41ce8fbdc98
650THz <vlad.hashencko@gmail.com>
2022-10-08 02:09:34 +0200

    Main

--- mainprocess.py
+++ mainprocess.py
@@ -9,10 +9,10 @@ class Line():
     B_y = 0
 
     def save(self):
-        file = open('ProjectFinance\Data\MainData.txt','r')
-        temp = file.read()
+        GraphData = open('ProjectFinance\Data\GraphData.txt','r')
+        temp = GraphData.read()
         Data = temp.split('/')
-        file = open('ProjectFinance\Data\MainData.txt','w')
+        GraphData = open('ProjectFinance\Data\GraphData.txt','w')
         for i in range(0, 19):
             Data[i*2] = Data[(i+1)*2]
             Data[i*2 + 1] = Data[(i+1)*2 + 1]
@@ -23,16 +23,16 @@ class Line():
             if i != 0:
                 temp += Data[i*2] + '/'
             temp += Data[i*2 + 1] + '/'
-        file.write(temp)
-        file.close()
+        GraphData.write(temp)
+        GraphData.close()
     
     def load(self, time_x):
-        file = open('ProjectFinance\Data\MainData.txt','r')
-        temp = file.read()
+        GraphData = open('ProjectFinance\Data\GraphData.txt','r')
+        temp = GraphData.read()
         Data = temp.split('/')
         self.A_y = int(Data[2*time_x])
         self.B_y = int(Data[2*time_x + 1])
-        file.close()
+        GraphData.close()
         return self
 
 
@@ -42,11 +42,15 @@ class ProcessData():
 
 
 def MainProcess():
-    Data = Line()
-    Data = Data.load(19)
-    temp = Data.A_y
-    Data.A_y += random.randrange(-10, 10)
-    Data.B_y = temp
-    Data.save()
+    DirectionData = open('ProjectFinance\Data\GraphDirectionData.txt', 'r')
+    temp = DirectionData.read()
+    Direction = temp.split('/')
+    if Direction[0] == 0:
+        LastLine = Line()
+        LastLine = LastLine.load(19)
+        MaxChange = int(LastLine.B_y/100*20)
+        NextPoint_y = LastLine.B_y + random.randrange(-MaxChange, MaxChange)
+        StepsToNextPoint = random.randrange(1, 4)
+
     
 
